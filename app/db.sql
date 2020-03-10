3d74064c40576947adbb6166177a87e11dc54bed
Crihstian Molina <ccmolina476@misena.edu.co>
2020-03-10 11:44:16 -0500

    Update Error Consultas desde Linux

--- app/db.sql
+++ app/db.sql
@@ -21,7 +21,7 @@ create table Compra_Detalle (Id_Compra int (11), Id_Producto int(6),Cantidad int
 
 /*Creo los indices a usar*/
 alter table Categoria add index (Nombre_Cat);
-alter table proveedores add index (Nombre_Prov);
+alter table Proveedores add index (Nombre_Prov);
 /*Creo las relaciones*/
 alter table Productos add constraint Productos_NombreCat_fk foreign key (Nombre_Cat) references Categoria (Nombre_Cat);
 alter table Productos add constraint Productos_Proveedor_fk foreign key (Proveedor) references Proveedores (Nombre_Prov);
@@ -29,22 +29,22 @@ alter table Producto_Proveedor add constraint Producto_Proveedor_Producto_fk for
 alter table Producto_Proveedor add constraint Producto_Proveedor_Proveedores_fk foreign key (Id_Proveedor) references Proveedores (Id_Proveedor);
 alter table Venta_Detalle add constraint Venta_Detalle_Venta_Cabecera_fk foreign key (Id_Venta) references Venta_Cabecera (Id_Venta);
 alter table Venta_Detalle add constraint Venta_Detalle_Producto_fk foreign key (Id_Producto) references Productos (Id_Producto);
-alter table Venta_cabecera add constraint Venta_cabecera_Cliente_fk foreign key (Id_Cliente) references Clientes (Id_Cliente);
+alter table Venta_Cabecera add constraint Venta_cabecera_Cliente_fk foreign key (Id_Cliente) references Clientes (Id_Cliente);
 alter table Compra_Detalle add constraint Compra_Detalle_Compra_Cabecera_fk foreign key (Id_Compra) references Compra_Cabecera (Id_Compra);
 alter table Compra_Detalle add constraint Compra_Detalle_Producto_fk foreign key (Id_Producto) references Productos (Id_Producto);
 alter table Compra_Detalle add constraint Compra_Detalle_Proveedor_fk foreign key (Id_Proveedor) references Proveedores (Id_Proveedor);
 
 /*Insertar datos Proveedores*/
 
-insert into proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('1','SanDisk','Milpitas, California, Estados Unidos','support@sandisk.com','888 527 1441');
-insert into proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('2','Kingston','Fountain Valley, California, Estados Unidos','support@kingston.com','7144452894');
-insert into proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('3','ADATA','Nuevo Taipéi, República de China','support@adata.com','9562112548');
-insert into proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('4','DELL','Round Rock, Texas, Estados Unidos','support@dell.com','8006249896');
-insert into proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('5','HP','Palo Alto, CA 94304 United States','support@hp.com','6508571501');
-insert into proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('6','Toshiba','Minato, Tokio, Japón','support@toshiba.com','2125960600');
-insert into proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('7','Lenovo','Quarry Bay, Hong Kong','support@lenovo.com','8552536686');
-insert into proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('8','JBL','San Francisco, CA','support@jbl.com','8245772351');
-insert into proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('9','SONY','San Francisco, CA','support@sonyes.com','9382815634');
+insert into Proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('1','SanDisk','Milpitas, California, Estados Unidos','support@sandisk.com','888 527 1441');
+insert into Proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('2','Kingston','Fountain Valley, California, Estados Unidos','support@kingston.com','7144452894');
+insert into Proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('3','ADATA','Nuevo Taipéi, República de China','support@adata.com','9562112548');
+insert into Proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('4','DELL','Round Rock, Texas, Estados Unidos','support@dell.com','8006249896');
+insert into Proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('5','HP','Palo Alto, CA 94304 United States','support@hp.com','6508571501');
+insert into Proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('6','Toshiba','Minato, Tokio, Japón','support@toshiba.com','2125960600');
+insert into Proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('7','Lenovo','Quarry Bay, Hong Kong','support@lenovo.com','8552536686');
+insert into Proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('8','JBL','San Francisco, CA','support@jbl.com','8245772351');
+insert into Proveedores(Id_Proveedor,Nombre_Prov,Direccion,Email,Telefonos) values('9','SONY','San Francisco, CA','support@sonyes.com','9382815634');
 
 
 /*Insertar datos Categorias*/
